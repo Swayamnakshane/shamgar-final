aced671aaba3373f5679739328b2695a4aba10f4
Ubuntu <ubuntu@ip-172-31-2-115.us-east-2.compute.internal>
2025-06-26 20:32:55 +0000

    added docker file for fronend and backend and docker compose

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,21 @@
 import os
 from dotenv import load_dotenv
+import urllib.parse
 
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = urllib.parse.quote_plus(os.getenv('MYSQL_PASSWORD'))
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_DB = os.getenv('MYSQL_DB')
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+
--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app.models import db
+import os
 
 app = create_app()
 
@@ -7,6 +8,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         print("✅ Database tables created successfully (if they didn't exist).")
-    app.run(debug=True)
-    
-    
+    app.run(host="0.0.0.0", port=5000, debug=os.getenv("FLASK_DEBUG", "False") == "True")
+   
